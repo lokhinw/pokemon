be8cfc9bd66a298d8f5c41be237acbd03ea4f680
Lokhin Wu <lokhin.wu@live.com>
2018-08-04 15:15:32 -0400

    setup console

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+connections = []
+
+const getCurrentTime = () =>
+  new Date()
+    .toISOString()
+    .replace(/T/, " ")
+    .replace(/\..+/, "");
+
 server.listen(process.env.PORT || 3000, () => {
-  console.log("Server started...");
+  console.log(`${getCurrentTime()} - SERVER STARTED LISTENING ON PORT ${process.env.PORT || 3000}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", socket => {
-  socket.emit("hello", { data: "Hello World!" });
+  connections.push(socket);
+  console.log(`${getCurrentTime()} - USER HAS CONNECTED`);
+  console.log(`${getCurrentTime()} - TOTAL CONNECTIONS: ${connections.length}`);
+
+  socket.on("disconnect", () => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`${getCurrentTime()} - USER HAS DISCONNECTED`);
+    console.log(`${getCurrentTime()} - TOTAL CONNECTIONS: ${connections.length}`);
+  });
+
 });
