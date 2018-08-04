70265f25afa3d324109fd3d371d4cb21aa4379c2
Lokhin Wu <lokhin.wu@live.com>
2018-08-04 09:44:20 -0400

    Setup basic socket

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-app.get("/", function (req, res) {
-    res.send("Hello World");
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server started...");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Server started...");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+io.on("connection", socket => {
+  socket.emit("hello", { data: "Hello World!" });
+});
