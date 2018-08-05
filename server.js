05e8df2335693a29cca7f8894a803774042b7327
Lokhin Wu <lokhin.wu@live.com>
2018-08-05 11:20:08 -0400

    Use ejs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+const express = require("express")
+const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
@@ -19,6 +20,9 @@ const searchForRoom = room => {
   }
 };
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 server.listen(process.env.PORT || 3000, () => {
   console.log(
     `${getCurrentTime()}: SERVER STARTED LISTENING ON PORT ${process.env.PORT ||
@@ -27,7 +31,11 @@ server.listen(process.env.PORT || 3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
+});
+
+app.post("/play", (req, res) => {
+  res.render("play");
 });
 
 io.on("connection", socket => {
@@ -35,10 +43,12 @@ io.on("connection", socket => {
   console.log(`${getCurrentTime()}: USER HAS CONNECTED`);
   console.log(`${getCurrentTime()}: TOTAL CONNECTIONS: ${connections.length}`);
 
+
   socket.on("join room", data => {
     if (!searchForRoom(data.room)) {
       rooms.push(data);
       socket.join(data.room);
+      socket.emit("join game");
       console.log(
         `${getCurrentTime()}: ${data.users[0].toUpperCase()} HAS JOINED ROOM ${
           data.room
@@ -48,12 +58,15 @@ io.on("connection", socket => {
       if (searchForRoom(data.room).users.length <= 1 && searchForRoom(data.room).users[0] != data.users[0]) {
         searchForRoom(data.room).users.push(data.users[0]);
         socket.join(data.room);
+        socket.emit("join game");
       } else {
         searchForRoom(data.room).users.length >= 2 ? socket.emit("error message", "room is full") : socket.emit("error message", "username is already taken");
       }
+      socket.emit("hello", "hello world!");
     }
-    console.log(rooms)
     io.to(data.room).emit("hello", "hello world!");
+
+    console.log(rooms)
   });
 
   socket.on("disconnect", () => {
